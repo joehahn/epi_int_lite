aa5f0679512f82269cb0efca9431234ce5d0b5d9
joehahn <jmh.datasciences@gmail.com>
2017-09-27 11:44:39 -0500

    python

--- README.md
+++ README.md
@@ -5,6 +5,35 @@ jmh.datasciences@gmail.com,<br />
 3 August 2017<br />
 git branch=master
 
+###Intro:
+
+blah blah blah...
+
+### Technical Notes:
+
+1 Install latest conda v4.3.21:
+
+    wget https://repo.continuum.io/miniconda/Miniconda2-4.3.21-MacOSX-x86_64.sh
+    chmod +x ./Miniconda2-4.3.21-MacOSX-x86_64.sh
+    rm -rf /Users/joe/miniconda2
+    ./Miniconda2-4.3.21-MacOSX-x86_64.sh -b -p /Users/joe/miniconda2
+
+add this to ~/.bash_profile
+
+    export PATH="/Users/joe/miniconda2/bin:$PATH"
+    echo $(conda --version)
+
+and then install these libraries:
+
+    conda install -y ipython
+    conda install -y scipy
+    conda install -y pandas
+    conda install -y matplotlib
+    #conda install -y seaborn
+    #conda install -y jupyter
+    #conda install -y jupyter_dashboards -c conda-forge
+
+
 ### Intro:
 
 
--- nbody.py
+++ nbody.py
@@ -4,26 +4,15 @@
 #
 #this...
 
-#to execute in pyspark's ipython shell:
-#    PYSPARK_DRIVER_PYTHON=/emr/miniconda2/bin/ipython pyspark --master yarn --num-executors 6 --executor-cores 1 --executor-memory 4G
-#to run locally on master node....can have memory issues...:
-#    PYSPARK_PYTHON=/emr/miniconda2/bin/python spark-submit --master local[*] --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./log4j.properties" nbody.py
-#to submit spark job to yarn:
-#74 sec#cache every output# PYSPARK_PYTHON=/emr/miniconda2/bin/python spark-submit --master yarn --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./log4j.properties" nbody.py
-#53 sec#cache every output# PYSPARK_PYTHON=/emr/miniconda2/bin/python spark-submit --master local[*] --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./log4j.properties" nbody.py
-#48 sec#cache every output# PYSPARK_PYTHON=/emr/miniconda2/bin/python spark-submit --master local[1] --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./log4j.properties" nbody.py
-#42 sec#cache every dt    # PYSPARK_PYTHON=/emr/miniconda2/bin/python spark-submit --master local[1] --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./log4j.properties" nbody.py
-#42 sec#no cache          # PYSPARK_PYTHON=/emr/miniconda2/bin/python spark-submit --master local[1] --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:./log4j.properties" nbody.py
-
 #set number of streamlins and particles per streamline
-number_of_streamlines = 2
-particles_per_streamline = 5
+number_of_streamlines = 3
+particles_per_streamline = 2
 number_of_particles = number_of_streamlines*particles_per_streamline
 
 #set timestamp, timesteps per output, and total number of outputs
 dt = 0.1
-timesteps_per_output = 3
-total_number_of_outputs = 10
+timesteps_per_output = 5
+total_number_of_outputs = 3
 
 #radial width assuming circular orbits
 radial_width = 1.0e-3
@@ -42,25 +31,20 @@ def Omega(a):
     Omega2 = GM/a/a/a
     Omega = Omega2**0.5
     return Omega
-from pyspark.sql.functions import udf
-from pyspark.sql.types import *
-Omega_udf = udf(Omega, DoubleType())
 
 #epicyclic frequency
 def Kappa(a):
     return Omega(a)
-Kappa_udf = udf(Kappa, DoubleType())
 
 #adjust angles to live between -Pi and Pi
-import math
+import numpy as np
 def adjust_angle(angle):
-    twopi = 2.0*math.pi
-    if (angle > math.pi):
+    twopi = 2.0*np.pi
+    if (angle > np.pi):
         angle -= twopi
-    if (angle < -math.pi):
+    if (angle < -np.pi):
         angle += twopi     
     return angle
-adjust_angle_udf = udf(adjust_angle, DoubleType())
 
 #drift step advances M and timestep
 def drift(df, dt):
@@ -104,6 +88,10 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('nbody').getOrCreate()
 
 #initialize particles in circular orbits
+a = np.linspace(1.0, 1.0 + radial_width, num=number_of_streamlines)
+
+a = np.zeros((number_of_streamlines, particles_per_streamline))
+
 from pyspark.sql.types import Row
 ids = [Row(id=idx) for idx in range(number_of_particles)]
 df = spark.createDataFrame(ids)
