34b05de3149bfe0a9e61d6e42f91cf017d997ae1
joehahn <jmh.datasciences@gmail.com>
2018-05-03 01:36:15 -0500

    confining

--- tests/self_confining/inputs.py
+++ tests/self_confining/inputs.py
@@ -7,26 +7,26 @@
 #define input input parameters
 
 #set number of streamlins and particles per streamline
-number_of_streamlines = 5
+number_of_streamlines = 6
 particles_per_streamline = 31
 number_of_particles = number_of_streamlines*particles_per_streamline
 
 #set timestamp, timesteps per output, and total number of outputs
 dt = 0.2
-timesteps_per_output = 500
+timesteps_per_output = 250
 total_number_of_outputs = 1000
 
 #ring radial width assuming circular orbits
 radial_width = 1.0e-3
 
 #total ring mass
-total_ring_mass = 1.0e-10
+total_ring_mass = 4.0e-9
 
 #ring's gravitation constant is usually G_ring=1 but set G_ring < 0 to turn off ring gravity
 G_ring = -1.0
 
 #ring kinematic shear viscosity, set shear_viscosity < 0 to turn off
-shear_viscosity = 1.0e-11
+shear_viscosity = 2.0e-11
 
 #ring pressure scales with Toomre's Q_ring, set Q_ring < 0 to turn off
 Q_ring = -1.0
@@ -41,8 +41,8 @@ J2 = 0.01
 #    'adeda':0.100         #I =  0.0569241269954
 initial_orbits = {
     'shape':'eccentric',
-    'e':5.0e-4,
-    'adeda':0.0
+    'e':1.0e-3,
+    'adeda':0.0 #e' grows when adeda=0.001 and shrinks when adeda=0.01
 }
 
 #output folder
