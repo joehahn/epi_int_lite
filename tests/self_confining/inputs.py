e76179fb05adf42f2db63ce349bcc5b01ef6dd7e
joehahn <jmh.datasciences@gmail.com>
2018-06-27 22:33:36 -0500

    angular mom flux

--- tests/self_confining/inputs.py
+++ tests/self_confining/inputs.py
@@ -8,12 +8,12 @@
 
 #set number of streamlins and particles per streamline
 number_of_streamlines = 2
-particles_per_streamline = 201
+particles_per_streamline = 61
 number_of_particles = number_of_streamlines*particles_per_streamline
 
 #set timestamp, timesteps per output, and total number of outputs
 dt = 0.2
-timesteps_per_output = 7000
+timesteps_per_output = 700
 total_number_of_outputs = 1000
 
 #ring radial width assuming circular orbits
@@ -45,3 +45,4 @@ initial_orbits = {
 
 #output folder
 output_folder = 'output'
+
