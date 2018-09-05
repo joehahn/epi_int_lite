9da2b97430b1295261dc3b72192ee2c994f32650
joehahn <jmh.datasciences@gmail.com>
2018-09-04 23:43:17 -0500

    2stream

--- tests/two_streamlines/inputs.py
+++ tests/two_streamlines/inputs.py
@@ -8,12 +8,12 @@
 
 #set number of streamlins and particles per streamline
 number_of_streamlines = 2
-particles_per_streamline = 31
+particles_per_streamline = 11
 number_of_particles = number_of_streamlines*particles_per_streamline
 
 #set timestamp, timesteps per output, and total number of outputs
 dt = 0.2
-timesteps_per_output = 20000
+timesteps_per_output = 3000
 total_number_of_outputs = 1000
 
 #ring radial width assuming circular orbits
@@ -27,8 +27,7 @@ G_ring = 1.0
 
 #ring kinematic shear viscosity, set shear_viscosity < 0 to turn off
 #shear_viscosity = 3.0e-13#...da=0.0018 and hair=-0.06 rad at t=10x10^4 orbits
-shear_viscosity = 1.0e-11
-shear_viscosity = 3.0e-12
+shear_viscosity = 3.0e-11
 
 #ring pressure scales with Toomre's Q_ring, set Q_ring < 0 to turn off
 Q_ring = -1.0
@@ -39,8 +38,8 @@ J2 = 0.01
 
 #choose ringlet's initial orbits..adeda = eccentricity gradient = a*(de/da)
 initial_orbits = {
-    'shape':'eccentric',
-    'e':5.0e-3,
+    'shape':'circular',
+    'e':0.0,
     'adeda':0.0
 }
 
