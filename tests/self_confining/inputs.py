6b91f0fa7b79921d93d38d31d26c2f30311deb77
joehahn <jmh.datasciences@gmail.com>
2018-12-26 10:27:52 -0600

    tests

--- tests/self_confining/inputs.py
+++ tests/self_confining/inputs.py
@@ -35,7 +35,7 @@ Q_ring = -1.0
 Rp = 0.5
 J2 = 0.01
 
-#choose ringlet's initial orbits..adeda = eccentricity gradient = a*(de/da)
+#choose ringlet's initial orbits
 initial_orbits = {
     'shape':'eccentric',
     'e':5.0e-3,
