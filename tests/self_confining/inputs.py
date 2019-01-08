c676567bd49a6ca275c99a8d25ea1e7e9b91e197
joehahn <jmh.datasciences@gmail.com>
2019-01-08 01:20:20 -0500

    test

--- tests/self_confining/inputs.py
+++ tests/self_confining/inputs.py
@@ -13,7 +13,7 @@ number_of_particles = number_of_streamlines*particles_per_streamline
 
 #set timestamp, timesteps per output, and total number of outputs
 dt = 0.2
-timesteps_per_output = 120000
+timesteps_per_output = 32000
 total_number_of_outputs = 1000
 
 #ring radial width assuming circular orbits
@@ -26,7 +26,7 @@ total_ring_mass = 1.5e-09
 G_ring = 1.0
 
 #ring kinematic shear viscosity, set shear_viscosity < 0 to turn off
-shear_viscosity = 1.0e-13
+shear_viscosity = 3.0e-13
 
 #ring pressure scales with Toomre's Q_ring, set Q_ring < 0 to turn off
 Q_ring = -1.0
