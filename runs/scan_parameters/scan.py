9d94387456b66a9296231bc0e8e25bd83809eae6
joehahn <jmh.datasciences@gmail.com>
2020-08-16 11:35:08 -0500

    scan

--- runs/scan_parameters/scan.py
+++ runs/scan_parameters/scan.py
@@ -11,9 +11,9 @@ N_processes = 7
 
 #generate range of logarithmically-spaced values for total_ring_mass
 import numpy as np
-mass_min = 2.0e-10
-mass_max = 2.0e-8
-N_masses = 11
+mass_min = 1.0e-10
+mass_max = 1.0e-8
+N_masses = 10
 total_ring_mass = np.exp(np.linspace(np.log(mass_min), np.log(mass_max), num=N_masses))
 print 'total_ring_mass = ', total_ring_mass.tolist()
 
@@ -22,9 +22,9 @@ radial_width = np.array([0.0005])
 print 'radial_width = ', radial_width.tolist()
 
 #assume viscosity values
-viscosity_min = 2.0e-13
-viscosity_max = 2.0e-10
-N_viscosities = 21
+viscosity_min = 1.0e-13
+viscosity_max = 1.0e-10
+N_viscosities = 20
 shear_viscosity = np.exp(np.linspace(np.log(viscosity_min), np.log(viscosity_max), num=N_viscosities))
 print 'shear_viscosity = ', shear_viscosity.tolist()
 
