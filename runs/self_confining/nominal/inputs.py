bb27847b9c459407ed9fdeb2e67727af32b95838
joehahn <jmh.datasciences@gmail.com>
2020-10-15 22:53:29 -0500

    nominal

--- runs/self_confining/nominal/inputs.py
+++ runs/self_confining/nominal/inputs.py
@@ -12,8 +12,8 @@ particles_per_streamline = 241
 number_of_particles = number_of_streamlines*particles_per_streamline
 
 #set timestamp, timesteps per output, and total number of outputs
-dt = 0.5*2
-timesteps_per_output = 300
+dt = 0.5
+timesteps_per_output = 600
 total_number_of_outputs = 1000
 
 #ring radial width assuming circular orbits
