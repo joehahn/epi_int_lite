853d11b8abfe8c675429d51cb852467ea1fadd3b
joehahn <jmh.datasciences@gmail.com>
2022-06-26 18:23:23 -0500

    flux

--- source/fluxes.py
+++ source/fluxes.py
@@ -8,74 +8,47 @@ import numpy as np
 from helper_fns import get_lambda, accelerations
 
 #compute angular momentum flux and luminosity
-def calc_ang_mom_flux(total_ring_mass, number_of_streamlines, particles_per_streamline, J2, Rp, G_ring, 
+def calculate_angular_momentum_flux(total_ring_mass, number_of_streamlines, particles_per_streamline, J2, Rp, G_ring, 
         shear_viscosity, bulk_viscosity, c, r, t, vr, vt, wt, times, fast_gravity, confine_edges):
     angular_momentum_flux_list = []
     angular_momentum_luminosity_list = []
-    for t_idx, time_ in enumerate(times):
+    for t_idx, _ in enumerate(times):
         #compute one-sided acceleration=acceleration of middle streamline due to interior neighbor
         r_now = r[t_idx]
         t_now = t[t_idx]
         vr_now = vr[t_idx]
         vt_now = vt[t_idx]
+        wt_now = wt[t_idx]
         #streamlines' linear density 
         lambda_ = get_lambda(total_ring_mass, number_of_streamlines, J2, Rp, r_now, t_now, vr_now, vt_now)
-        #preserve only two middlemost streamlines, to force acceleations() to compute one-sided acceleration
+        #preserve only two middlemost streamlines, to force acceleations() to compute one-sided accelerations
         middle_index = number_of_streamlines/2
-        r_now = r_now[middle_index-1:middle_index+1,:]
-        t_now = t_now[middle_index-1:middle_index+1,:]
-        vr_now = vr_now[middle_index-1:middle_index+1,:]
-        vt_now = vt_now[middle_index-1:middle_index+1,:]
-        #accelerations and angular_momentum_flux
-        Ar, At = accelerations(lambda_, G_ring, shear_viscosity, bulk_viscosity, c, r_now, t_now, vr_now, vt_now, fast_gravity, confine_edges)
-        angular_momentum_flux = lambda_*r_now*At
-        angular_momentum_flux_list += [angular_momentum_flux]
-        #compute one-sided torque
-        delta_ell = 2*np.pi*(r_now[-1].mean())/particles_per_streamline #particles mean tangential separation
-        torque = angular_momentum_flux.sum()*delta_ell
-        angular_momentum_luminosity_list += [torque]
-    #convert lists to arrays
-    angular_momentum_flux = np.array(angular_momentum_flux_list)
-    angular_momentum_luminosity = np.array(angular_momentum_luminosity_list)
-    return angular_momentum_flux, angular_momentum_luminosity
-
-#compute angular momentum flux and luminosity
-def calculate_angular_momentum_flux(lambda0, G_ring, shear_viscosity, bulk_viscosity, c, r, t, vr, vt, wt, 
-        fast_gravity, confine_edges):
-    #select two central streamlines
-    total_number_of_outputs, number_of_streamlines, particles_per_streamline = r.shape
-    middle_index = number_of_streamlines/2
-    rc = r[:, middle_index-1:middle_index+1,:]
-    tc = t[:, middle_index-1:middle_index+1,:]
-    vrc = vr[:, middle_index-1:middle_index+1,:]
-    vtc = vt[:, middle_index-1:middle_index+1,:]
-    wtc = wt[:, middle_index-1:middle_index+1,:]
-    lambdac = lambda0[middle_index-1:middle_index+1,:]
-    #compute angular momentum flux and luminosity
-    twopi = 2*np.pi
-    angular_momentum_flux_list = []
-    angular_momentum_luminosity_list = []
-    for t_idx in range(total_number_of_outputs):
-        #compute one-sided acceleration=acceleration of middle streamline due to interior neighbor
-        r_now = rc[t_idx]
-        t_now = tc[t_idx]
-        vr_now = vrc[t_idx]
-        vt_now = vtc[t_idx]
-        Ar, At = accelerations(lambdac, G_ring, shear_viscosity, bulk_viscosity, c, r_now, t_now, 
-           vr_now, vt_now, fast_gravity, confine_edges)
-        angular_momentum_flux = lambdac*r_now*At
+        rc = r_now[middle_index-1:middle_index+1,:]
+        tc = t_now[middle_index-1:middle_index+1,:]
+        vrc = vr_now[middle_index-1:middle_index+1,:]
+        vtc = vt_now[middle_index-1:middle_index+1,:]
+        wtc = wt_now[middle_index-1:middle_index+1,:]
+        #accelerations
+        Ar, At = accelerations(lambda_, G_ring, shear_viscosity, bulk_viscosity, c, rc, tc, vrc, vtc, fast_gravity, confine_edges)
+        #angular_momentum_flux
+        angular_momentum_flux = lambda_*rc*At
         angular_momentum_flux = angular_momentum_flux[-1]
         angular_momentum_flux_list += [angular_momentum_flux]
-        #compute one-sided torque
-        delta_ell = twopi*r_now[-1].mean()/particles_per_streamline
-        torque = angular_momentum_flux.sum()*delta_ell
+        #torque=angular momentum luminosity
+        m1 = total_ring_mass/(number_of_streamlines*particles_per_streamline)
+        torque = m1*rc*At
+        torque = torque[-1]
+        torque = torque.sum()
         angular_momentum_luminosity_list += [torque]
     #convert lists to arrays
     angular_momentum_flux = np.array(angular_momentum_flux_list)
     angular_momentum_luminosity = np.array(angular_momentum_luminosity_list)
-    #select central streamline
+    #preserve two central streamlines
+    rc = r[:, middle_index-1:middle_index+1,:]
     rc = rc[:, -1, :]
+    tc = t[:, middle_index-1:middle_index+1,:]
     tc = tc[:, -1, :]
+    wtc = wt[:, middle_index-1:middle_index+1,:]
     wtc = wtc[:, -1, :]
     return angular_momentum_flux, angular_momentum_luminosity, rc, tc, wtc
 
@@ -117,5 +90,3 @@ def calculate_energy_flux(lambda0, G_ring, shear_viscosity, bulk_viscosity, c, r
     tc = tc[:, -1, :]
     wtc = wtc[:, -1, :]
     return energy_flux, energy_luminosity, rc, tc, wtc
-
-
