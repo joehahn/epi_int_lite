4e441cc95c7f851762d3b330d276f4985b7abe77
joehahn <jmh.datasciences@gmail.com>
2023-05-14 14:49:09 -0500

    tests

--- tests/timestep/2.0/inputs.py
+++ tests/timestep/2.0/inputs.py
@@ -12,27 +12,27 @@ particles_per_streamline = 241
 number_of_particles = number_of_streamlines*particles_per_streamline
 
 #set timestamp, timesteps per output, and total number of outputs
-dt = 0.2*10
-timesteps_per_output = 36000/10
+dt = 0.5*4
+timesteps_per_output = 1300/4
 total_number_of_outputs = 1000
 
 #ring radial width assuming circular orbits
-radial_width = 0.0005
+radial_width = 0.0001
 
 #total ring mass
-total_ring_mass = 1.5e-09
+total_ring_mass = 1.0e-10
 
 #ring's gravitation constant is usually G_ring=1 but set G_ring < 0 to turn off ring gravity.
-#Also set fast_gravity=True for approximate gravity that is 2x faster and almost as accurate.
+#Also set fast_gravity=False since there is very little speed benefit when the fast_gravity approximation is used
 G_ring = 1.0
 fast_gravity = False
 
 #ring kinematic shear and bulk viscosity, set < 0 to turn off
-shear_viscosity = 3.0e-13
-bulk_viscosity = shear_viscosity
+shear_viscosity = 1.0e-13
+bulk_viscosity = 1.0*shear_viscosity
 
-#ring kinematic bulk viscosity, set bulk_viscosity < 0 to turn off
-bulk_viscosity = shear_viscosity
+#add fictitious torque at inner and outer streamlines, to oppose any radial spreading
+confine_edges = False
 
 #ring pressure scales with Toomre's Q_ring, set Q_ring < 0 to turn off
 Q_ring = -1.0
@@ -44,10 +44,24 @@ J2 = 0.01
 #choose ringlet's initial orbits
 initial_orbits = {
     'shape':'eccentric',
-    'e':5.0e-3,
+    'e':0.01,
     'e_prime':0.0,
     'w_prime':0.0
 }
 
 #output folder
 output_folder = 'output'
+
+#parse any optional commandline modifications to the above, note these modifications will be ignored by Jupyter
+import argparse
+import json
+parser = argparse.ArgumentParser() 
+parser.add_argument('-m', '--modified_params', type=str, dest='modified_params', required=False)
+args, unknown_args = parser.parse_known_args()
+modified_params = None
+if (args.modified_params):
+    modified_params_str = args.modified_params
+    modified_params = json.loads(modified_params_str)
+    print 'modified_params =', modified_params
+    for key, val in modified_params.iteritems():
+        exec(key + '=val')
--- tests/timestep/5.0/inputs.py
+++ tests/timestep/5.0/inputs.py
@@ -12,8 +12,8 @@ particles_per_streamline = 241
 number_of_particles = number_of_streamlines*particles_per_streamline
 
 #set timestamp, timesteps per output, and total number of outputs
-dt = 0.5/5.0
-timesteps_per_output = int(1300*5.0)
+dt = 0.5*10
+timesteps_per_output = 1300/10
 total_number_of_outputs = 1000
 
 #ring radial width assuming circular orbits
