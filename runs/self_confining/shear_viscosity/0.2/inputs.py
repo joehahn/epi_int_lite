f5868a2fcd0042c9090a291841b047e6f733fac4
joehahn <jmh.datasciences@gmail.com>
2023-07-14 10:44:02 -0500

    shear_viscosity

--- runs/self_confining/shear_viscosity/0.2/inputs.py
+++ runs/self_confining/shear_viscosity/0.2/inputs.py
@@ -13,23 +13,26 @@ number_of_particles = number_of_streamlines*particles_per_streamline
 
 #set timestamp, timesteps per output, and total number of outputs
 dt = 0.5
-timesteps_per_output = 2100*5
+timesteps_per_output = 2550
 total_number_of_outputs = 1000
 
 #ring radial width assuming circular orbits
-radial_width = 0.0005
+radial_width = 0.0001
 
 #total ring mass
-total_ring_mass = 2.0e-09
+total_ring_mass = 1.0e-10
 
 #ring's gravitation constant is usually G_ring=1 but set G_ring < 0 to turn off ring gravity.
-#Also set fast_gravity=True for approximate gravity that is 2x faster and almost as accurate.
+#Also set fast_gravity=False since there is very little speed benefit when the fast_gravity approximation is used
 G_ring = 1.0
-fast_gravity = True
+fast_gravity = False
 
 #ring kinematic shear and bulk viscosity, set < 0 to turn off
-shear_viscosity = 0.2*(1.0e-12)
-bulk_viscosity = 1.5*shear_viscosity
+shear_viscosity = 0.2e-13
+bulk_viscosity = 1.0*shear_viscosity
+
+#add fictitious torque at inner and outer streamlines, to oppose any radial spreading
+confine_edges = False
 
 #ring pressure scales with Toomre's Q_ring, set Q_ring < 0 to turn off
 Q_ring = -1.0
@@ -41,10 +44,24 @@ J2 = 0.01
 #choose ringlet's initial orbits
 initial_orbits = {
     'shape':'eccentric',
-    'e':0.03,
+    'e':0.01,
     'e_prime':0.0,
     'w_prime':0.0
 }
 
 #output folder
 output_folder = 'output'
+
+#parse any optional commandline modifications to the above, note these modifications will be ignored by Jupyter
+import argparse
+import json
+parser = argparse.ArgumentParser() 
+parser.add_argument('-m', '--modified_params', type=str, dest='modified_params', required=False)
+args, unknown_args = parser.parse_known_args()
+modified_params = None
+if (args.modified_params):
+    modified_params_str = args.modified_params
+    modified_params = json.loads(modified_params_str)
+    print 'modified_params =', modified_params
+    for key, val in modified_params.iteritems():
+        exec(key + '=val')
